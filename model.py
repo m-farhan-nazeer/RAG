d45bec956bc56f6df68a8d30954a262d3172315e
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-09-09 02:26:46 +0500

    model

--- model.py
+++ model.py
@@ -30,3 +30,27 @@ def query_news(indices):
     output = [NEWS_DATA[index] for index in indices]
 
     return output
+
+
+# The corpus used will be the title appended with the description
+corpus = [x['title'] + " " + x['description'] for x in NEWS_DATA]
+
+# Instantiate the retriever by passing the corpus data
+BM25_RETRIEVER = bm25s.BM25(corpus=corpus)
+
+# Tokenize the chunks
+tokenized_data = bm25s.tokenize(corpus)
+
+# Index the tokenized chunks within the retriever
+BM25_RETRIEVER.index(tokenized_data)
+
+# Tokenize the same query used in the previous exercise
+sample_query = "What are the recent news about GDP?"
+tokenized_sample_query = bm25s.tokenize(sample_query)
+
+# Get the retrieved results and their respective scores
+results, scores = BM25_RETRIEVER.retrieve(tokenized_sample_query, k=3)
+
+print(f"Results for query: {sample_query}\n")
+for doc in results[0]:
+  print(f"Document retrieved {corpus.index(doc)} : {doc}\n")
