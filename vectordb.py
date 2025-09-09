3782b75355bdb27c708579e37bab0d4621122204
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-09-09 22:41:48 +0500

    vector dbs

--- vectordb.py
+++ vectordb.py
@@ -27,3 +27,17 @@ with suppress_subprocess_output():
             "RERANKER_INFERENCE_API":"http://127.0.0.1:5000/" # The endpoint the weaviate API will be using to rerank
         }
     )
+
+
+
+data = joblib.load("data.joblib")
+print_object_properties(data[0])
+
+vectorizer_config = [Configure.NamedVectors.text2vec_transformers(
+                name="vector", # This is the name you will need to access the vectors of the objects in your collection
+                source_properties=['place', 'state', 'description', 'best_season_to_visit', 'attractions', 'budget'], # which properties should be used to generate a vector, they will be appended to each other when vectorizing
+                vectorize_collection_name = False, # This tells the client to not vectorize the collection name. 
+                                                   # If True, it will be appended at the beginning of the text to be vectorized
+                inference_url="http://127.0.0.1:5000", # Since we are using an API based vectorizer, you need to pass the URL used to make the calls 
+                                                       # This was setup in our Flask application
+            )]
