d65ae6342e39a45621f2751ed4e34acb28191d56
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-09-16 04:10:29 +0500

    cost_reduction

--- cost_management.py
+++ cost_management.py
@@ -3,3 +3,19 @@ from weaviate.classes.query import Filter
 import weaviate
 import joblib
 import pandas as pd
+
+
+import flask_app
+import weaviate_server
+import unittests
+import json
+from utils import (
+    ChatWidget, 
+    generate_with_single_input,
+    parse_json_output,
+    get_filter_by_metadata,
+    generate_filters_from_query,
+    process_and_print_query,
+    print_properties,
+    make_url
+)
